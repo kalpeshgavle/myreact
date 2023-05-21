3c76e7d160288828d39b5ada1db48cf07d7b7b1e
kalpesh <gavlekalpesh11@gmail.com>
2023-05-21 14:53:13 +0530

    react-redux-not working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { Route, Routes } from "react-router";
 import Navbar from "./component/Navbar";
 import CounterPage from "./pages/CounterPage";
+import Home from "./pages/Home";
 
 function App() {
 
-  // const { number, userName } = useSelector((state) => state.counter)
-  // useSelector((state) => console.log(state))
   return (
-    <div className="container">
+    <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}>Home</Route>
+      </Routes>
       <CounterPage />
-    </div>
+    </>
+
   );
 }
 
